b2bcd69f6ab7769697ddf6b198ee0a9845e7859d
Matt Weber <matt@awebersite.com>
2022-11-01 10:12:10 -0500

    hmmmmm

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { useQuery } from "@apollo/client";
 
-import { useStoreContext } from "../utils/GlobalState";
+import Cart from "../components/Cart";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -10,12 +11,13 @@ import {
   UPDATE_PRODUCTS,
 } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
-import spinner from "../assets/spinner.gif";
-import Cart from "../components/Cart";
 import { idbPromise } from "../utils/helpers";
 
+import spinner from "../assets/spinner.gif";
+
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
